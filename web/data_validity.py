9b6be5bbb3c820ccd6b489aaa69f2555a4aff480
GodfredAsa <goldenheroe1@yahoo.com>
2022-09-29 14:47:47 +0000

    implementing and running app using docker

---
+++ web/data_validity.py
@@ -0,0 +1,10 @@
+
+def check_posted_data(data, fxn_name):
+    func_methods = ["add", "subtract", "multiply", "division"]
+    if fxn_name in func_methods:
+        return 301 if "x" not in data or "y" not in data else 200
+
+        # if "x" not in data or "y" not in data:
+        #     return 301 # Missing parameter
+        # else:
+        #     return 200
